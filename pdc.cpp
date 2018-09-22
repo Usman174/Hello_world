ad78efda1c2455d1f642cc37a52350557d16b35e
Usman174 <43506412+Usman174@users.noreply.github.com>
2018-09-23 04:36:11 +0500

    Add files via upload

---
+++ pdc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 1;
+	int y = 0;
+	cout << "Enter a number " << endl;
+	cin >> y;
+
+	cout << x+y << endl;
+	cout << "Hello " << endl;
+}
